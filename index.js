3584c947aa2eccfffcae6dc80ff0077a01f3fdd2
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-04-23 01:01:07 -0300

    Implemented webhook

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Enable middleware parse data to json.
+app.use(express.json());
+
+app.post("/webhook", (request, response) => {
+    
+    console.log(request.body);
+    response.sendStatus(200);
+});
+
+app.listten(3000, () => console.log(`Server is running in address: http://localhost:3000`));
+
+
