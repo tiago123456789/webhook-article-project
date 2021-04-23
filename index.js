fdb6255430428a35b42740084e26be317a41209f
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-04-23 01:09:14 -0300

    Add verification request is valid

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+const token = "37b6352c-c951-4b43-993a-78138ede25c5";
 
 // Enable middleware parse data to json
 app.use(express.json()); 
 
 app.post("/webhook", (request, response) => {
-    console.log(request.body);
-    response.sendStatus(200);
+    const isRequestValid = request.query.token == token
+    if (isRequestValid) {
+        console.log(request.body);
+        return response.sendStatus(200);
+    }
+    response.sendStatus(401);
 });
 
 app.listen(3000, () => console.log(`Server is running in address: http://localhost:3000`));
